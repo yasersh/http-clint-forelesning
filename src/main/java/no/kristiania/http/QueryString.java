800333452186e3a48899d8204cf02f54c9c51b3e
yasersh <yaser.sharafaldin1234@gmail.com>
2021-09-12 13:10:17 +0200

    add test for missing pramertes (failing)

--- src/main/java/no/kristiania/http/QueryString.java
+++ src/main/java/no/kristiania/http/QueryString.java
@@ -1,10 +1,14 @@
 package no.kristiania.http;
 
 public class QueryString {
+    private final String parameterValue;
+
     public QueryString(String queryString) {
+        int equalPos= queryString.indexOf('=');
+        parameterValue= queryString.substring(equalPos+1);
     }
 
     public String getParameter(String status) {
-        return "200";
+        return parameterValue;
     }
 }
--- src/test/java/no/kristiania/http/QueryStringTest.java
+++ src/test/java/no/kristiania/http/QueryStringTest.java
@@ -3,6 +3,7 @@ package no.kristiania.http;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class QueryStringTest {
     @Test
@@ -14,5 +15,10 @@ public class QueryStringTest {
     void ShouldReserveStatusCode401(){
         QueryString queryString = new QueryString("status=401");
         assertEquals ("401",queryString.getParameter("status"));
+    }
+    @Test
+    void shouldReturnNullForMissingParameters(){
+        QueryString queryString = new QueryString("body=Hallo");
+        assertNull(queryString.getParameter("status"));
     }
 }
